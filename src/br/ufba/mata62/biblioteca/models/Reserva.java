93f4ceb9a8f8e78c0e1a07b643ca3795dd83bdab
Eduardo Baltazar <eduardosilveira@ufba.br>
2025-07-13 17:05:47 -0300

    Update Reserva.java

--- src/br/ufba/mata62/biblioteca/models/Reserva.java
+++ src/br/ufba/mata62/biblioteca/models/Reserva.java
@@ -1,4 +1,28 @@
 package br.ufba.mata62.biblioteca.models;
 
+import java.time.LocalDate;
+
+
 public class Reserva {
+    private final Usuario usuario;
+    private final Livro livro;
+    private final LocalDate dataReserva;
+
+    public Reserva(Usuario usuario, Livro livro) {
+        this.usuario = usuario;
+        this.livro = livro;
+        this.dataReserva = LocalDate.now(); // Garante a regra de negócio
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public LocalDate getDataReserva() {
+        return dataReserva;
+    }
 }
